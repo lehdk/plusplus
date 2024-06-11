c6c2cdcec20f589640adb37e9489efb2d485093c
Lehdk <leh99dk.mail@gmail.com>
2024-06-11 18:12:05 +0200

    Refactored files

---
+++ src/files.rs
@@ -0,0 +1,38 @@
+use std::{fs, path::PathBuf};
+use dirs::home_dir;
+
+#[path = "cli.rs"]
+mod cli;
+
+pub fn init() {
+    let result = fs::create_dir_all(dir());
+    result.expect("Error creating directory");
+
+    let _ = read_config();
+}
+
+pub fn dir() -> PathBuf {
+    let home = home_dir().expect("Could not find home directory");
+
+    home.join(".plusplus")
+}
+
+pub fn read_config() -> cli::Config {
+    match fs::read_to_string(dir().join("plusplus.config")) {
+        Ok(result) =>  {
+            serde_json::from_str(&result).unwrap()
+        }
+        Err(_) => {
+            let config = cli::Config { github_token: String::from("")};
+            write_config(&config);
+
+            config
+        },
+    }
+}
+
+pub fn write_config(config: &cli::Config) {
+    let json = serde_json::to_string(&config).unwrap();
+
+    let _ = fs::write(dir().join("plusplus.config"), json);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,23 @@
 use clap::Parser;
 use cli::{Commands, Root};
-use std::{fs, path::PathBuf};
-
-use dirs::home_dir;
 
 mod cli;
+mod github;
+mod files;
 
 fn gittoken(value: cli::GitTokenCommands) {
-    let mut config = read_config();
+    let mut config = files::read_config();
 
     match value {
         cli::GitTokenCommands::Get => print!("Token: {}\n", config.github_token),
         cli::GitTokenCommands::Set(token) => {
             config.github_token = token.token;
-            write_config(&config);
+            files::write_config(&config);
         }
     }
 }
-
-fn dir() -> PathBuf {
-    let home = home_dir().expect("Could not find home directory");
-
-    home.join(".plusplus")
-}
-
-fn init() {
-    let result = fs::create_dir_all(dir());
-    result.expect("Error creating directory");
-
-    let _ = read_config();
-}
-
-fn read_config() -> cli::Config {
-    match fs::read_to_string(dir().join("plusplus.config")) {
-        Ok(result) =>  {
-            serde_json::from_str(&result).unwrap()
-        }
-        Err(_) => {
-            let config = cli::Config { github_token: String::from("")};
-            write_config(&config);
-
-            config
-        },
-    }
-}
-
-fn write_config(config: &cli::Config) {
-    let json = serde_json::to_string(&config).unwrap();
-
-    let _ = fs::write(dir().join("plusplus.config"), json);
-}
-
 fn main() {
-    
-    init();
+    files::init();
 
     let opts = Root::parse();
 
